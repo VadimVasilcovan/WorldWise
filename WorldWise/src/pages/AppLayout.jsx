ff116d54399b394cea8c144262530d56cc96b6d3
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-24 20:34:59 +0300

    24.04.2025

--- WorldWise/src/pages/AppLayout.jsx
+++ WorldWise/src/pages/AppLayout.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import AppNav from '../assets/components/AppNav'
+import styles from './AppLayout.module.css'
+import Sidebar from '../assets/components/Sidebar'
+import Map from '../assets/components/Map'
 
 export default function AppLayout() {
   return (
-    <div>
-        <AppNav/>
-        <p>App</p>
+    <div className={styles.app}>
+       <Sidebar/>
+       <Map/>
     </div>
   )
 }
