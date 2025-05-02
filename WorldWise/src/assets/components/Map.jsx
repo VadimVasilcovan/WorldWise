7ba1258a12e6be2649711c2577dee1f8254e937d
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-05-02 11:28:15 +0300

    02.05.2025

--- WorldWise/src/assets/components/Map.jsx
+++ WorldWise/src/assets/components/Map.jsx
@@ -10,11 +10,18 @@ import {
 } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { useCities } from "../../contexts/CitiesContext";
+import { useGeolocation } from "../../hooks/useGeolocation";
+import Button from "./Button";
 
 export default function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams] = useSearchParams();
+  const {
+    isLoading: isLoadingPosition,
+    position: geolocationPosition,
+    getPosition,
+  } = useGeolocation();
   const mapLat = searchParams.get("lat");
   const mapLng = searchParams.get("lng");
 
@@ -24,8 +31,20 @@ export default function Map() {
     },
     [mapLat, mapLng]
   );
+
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
   return (
     <div className={styles.mapContainer}>
+   { !geolocationPosition && (<Button type="position" onClick={getPosition}>
+        {isLoadingPosition ? "Loading..." : "Use your position"}
+      </Button>)}
+
       <MapContainer
         center={mapPosition}
         /*  center={[mapLat, mapLng]} */
